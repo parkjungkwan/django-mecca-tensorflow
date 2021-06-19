c4f04f58cb9263d7a8297aaedebbcabb15915d5a
pakjkwan <pakjkwan@gmail.com>
2021-06-19 15:06:28 +0900

    code update 15

--- cctv_crime/modules.py
+++ cctv_crime/modules.py
@@ -4,6 +4,7 @@ import pandas as pd
 import numpy as np
 import json
 import googlemaps
+from sklearn import preprocessing
 @dataclass
 class File(object):
     context: str
@@ -190,7 +191,68 @@ class Service(Reader):
         # printer.dframe(cctv)
         file.fname = 'pop_in_seoul'
         pop = reader.xls(file, 2, 'B, D, G, J, N')
-        printer.dframe(pop)
+        # printer.dframe(pop)
+        cctv.rename(columns={cctv.columns[0]: '구별'},inplace=True)
+        pop.rename(columns={
+            pop.columns[0]: '구별',
+            pop.columns[1]: '인구수',
+            pop.columns[2]: '한국인',
+            pop.columns[3]: '외국인',
+            pop.columns[4]: '고령자'
+        }, inplace=True)
+        pop.drop([26],inplace=True)
+        pop['외국인비율'] = pop['외국인'].astype(int) / pop['인구수'].astype(int) * 100
+        pop['고령자비율'] = pop['고령자'].astype(int) / pop['인구수'].astype(int) * 100
+        cctv.drop(['2013년도 이전', '2014년', '2015년', '2016년'], 1, inplace=True)
+        cctv_pop = pd.merge(cctv, pop, on='구별')
+        cor1 = np.corrcoef(cctv_pop['고령자비율'], cctv_pop['소계'])
+        cor2 = np.corrcoef(cctv_pop['외국인비율'], cctv_pop['소계'])
+        print(f'고령자비율과 CCTV의 상관계수 {str(cor1)} \n'
+              f'외국인비율과 CCTV의 상관계수 {str(cor2)} ')
+        """
+         고령자비율과 CCTV 의 상관계수 [[ 1.         -0.28078554]
+                                     [-0.28078554  1.        ]] 
+         외국인비율과 CCTV 의 상관계수 [[ 1.         -0.13607433]
+                                     [-0.13607433  1.        ]]
+        r이 -1.0과 -0.7 사이이면, 강한 음적 선형관계,
+        r이 -0.7과 -0.3 사이이면, 뚜렷한 음적 선형관계,
+        r이 -0.3과 -0.1 사이이면, 약한 음적 선형관계,
+        r이 -0.1과 +0.1 사이이면, 거의 무시될 수 있는 선형관계,
+        r이 +0.1과 +0.3 사이이면, 약한 양적 선형관계,
+        r이 +0.3과 +0.7 사이이면, 뚜렷한 양적 선형관계,
+        r이 +0.7과 +1.0 사이이면, 강한 양적 선형관계
+        고령자비율 과 CCTV 상관계수 [[ 1.         -0.28078554] 약한 음적 선형관계
+                                    [-0.28078554  1.        ]]
+        외국인비율 과 CCTV 상관계수 [[ 1.         -0.13607433] 거의 무시될 수 있는
+                                    [-0.13607433  1.        ]]                        
+         """
+        cctv_pop.to_csv('./saved_data/cctv_pop.csv')
+
+    def save_police_norm(self):
+        file = self.file
+        reader = self.reader
+        printer = self.printer
+        file.context = './saved_data/'
+        file.fname = 'police_pos'
+        police_pos = reader.csv(file)
+        police = pd.pivot_table(police_pos, index='구별', aggfunc=np.sum)
+        police['살인검거율'] = (police['살인 검거'].astype(int) / police['살인 발생'].astype(int)) * 100
+        police['강도검거율'] = (police['강도 검거'].astype(int) / police['강도 발생'].astype(int)) * 100
+        police['강간검거율'] = (police['강간 검거'].astype(int) / police['강간 발생'].astype(int)) * 100
+        police['절도검거율'] = (police['절도 검거'].astype(int) / police['절도 발생'].astype(int)) * 100
+        police['폭력검거율'] = (police['폭력 검거'].astype(int) / police['폭력 발생'].astype(int)) * 100
+        police.drop(columns={'살인 검거', '강도 검거', '강간 검거', '절도 검거', '폭력 검거'}, axis=1, inplace=True)
+        for i in self.crime_rate_columns:
+            police.loc[police[i] > 100, 1] = 100  # 데이터값의 기간 오류로 100을 넘으면 100으로 계산
+        police.rename(columns={
+            '살인 발생': '살인',
+            '강도 발생': '강도',
+            '강간 발생': '강간',
+            '절도 발생': '절도',
+            '폭력 발생': '폭력'
+        }, inplace=True)
+        x = police[self.crime_rate_columns].values
+        min_max_scalar = preprocessing.MinMaxScaler()
 
 
 
