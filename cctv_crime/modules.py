4e7500d088dba1fd634647e3911ce730243b0afa
pakjkwan <pakjkwan@gmail.com>
2021-06-19 15:36:57 +0900

    update code 15

--- cctv_crime/modules.py
+++ cctv_crime/modules.py
@@ -5,6 +5,8 @@ import numpy as np
 import json
 import googlemaps
 from sklearn import preprocessing
+import folium
+
 @dataclass
 class File(object):
     context: str
@@ -253,17 +255,107 @@ class Service(Reader):
         }, inplace=True)
         x = police[self.crime_rate_columns].values
         min_max_scalar = preprocessing.MinMaxScaler()
+        """          
+        스케일링은 선형변환을 적용하여          
+        전체 자료의 분포를 평균 0, 분산 1이 되도록 만드는 과정
+        정규화 normalization         
+        많은 양의 데이터를 처리함에 있어 데이터의 범위(도메인)를 일치시키거나         
+        분포(스케일)를 유사하게 만드는 작업
+        """
+        x_scaled = min_max_scalar.fit_transform(x.astype(float))
+        police_norm = pd.DataFrame(x_scaled, columns=self.crime_columns, index=police.index)
+        police_norm[self.crime_rate_columns] = police[self.crime_rate_columns]
+        police_norm['범죄'] = np.sum(police_norm[self.crime_rate_columns], axis=1)
+        police_norm['검거'] = np.sum(police_norm[self.crime_columns], axis=1)
+        police_norm.to_csv('./saved_data/police_norm.csv', sep=',', encoding='UTF-8')
+
+    def folium_test(self):
+        file = self.file
+        reader = self.reader
+        printer = self.printer
+        file.context = './data/'
+        file.fname = 'us_unemployment'
+        state_data = reader.csv(file)
+        file.fname = 'us-states'
+        state_geo = reader.json(file)
+
+        bins = list(state_data["Unemployment"].quantile([0, 0.25, 0.5, 0.75, 1]))
+
+        m = folium.Map(location=[48, -102], zoom_start=3)
+
+        folium.Choropleth(
+            geo_data=state_geo,
+            data=state_data,
+            columns=["State", "Unemployment"],
+            key_on="feature.id",
+            fill_color="BuPu",
+            fill_opacity=0.7,
+            line_opacity=0.5,
+            legend_name="Unemployment Rate (%)",
+            bins=bins,
+            reset=True,
+        ).add_to(m)
+
+        m.save('./saved_data/Unemployment.html')
+
+    def draw_crime_map(self):
+        file = self.file
+        reader = self.reader
+        printer = self.printer
+        file.context = './saved_data/'
+        file.fname = 'police_norm'
+        police_norm = reader.csv(file)
+        file.context = './data/'
+        file.fname = 'geo_simple'
+        state_geo = reader.json(file)
+        file.fname = 'crime_in_seoul'
+        crime = reader.csv(file)
+        file.context = './saved_data/'
+        file.fname = 'police_pos'
+        police_pos = reader.csv(file)
+        station_names = []
+        for name in crime['관서명']:
+            station_names.append('서울' + str(name[:-1] + '경찰서'))
+        station_addrs = []
+        station_lats = []
+        station_lngs = []
+        gmaps = reader.gmaps()
+        for name in station_names:
+            temp = gmaps.geocode(name, language='ko')
+            station_addrs.append(temp[0].get('formatted_address'))
+            t_loc = temp[0].get('geometry')
+            station_lats.append(t_loc['location']['lat'])
+            station_lngs.append(t_loc['location']['lng'])
 
 
 
+        state_geo = None
+        state_data = None
 
+        m = folium.Map(location=[48, -102], zoom_start=3)
+
+        folium.Choropleth(
+            geo_data=state_geo,
+            data=state_data,
+            columns=["State", "Unemployment"],
+            key_on="feature.id",
+            fill_color="BuPu",
+            fill_opacity=0.7,
+            line_opacity=0.5,
+            legend_name="Unemployment Rate (%)",
+            reset=True,
+        ).add_to(m)
+
+        m.save('./saved_data/Unemployment.html')
 
 
 if __name__ == '__main__':
 
     s = Service()
     # s.save_police_pos()
-    s.save_cctv_pop()
+    # s.save_cctv_pop()
+    # s.save_police_norm()
+    s.folium_test()
 
 
 
