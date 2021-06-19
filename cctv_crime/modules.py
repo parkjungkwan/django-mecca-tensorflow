690c29930cdf019816d0e95cf51b9c4c6ce38832
pakjkwan <pakjkwan@gmail.com>
2021-06-19 12:53:45 +0900

    first commit

---
+++ cctv_crime/modules.py
@@ -0,0 +1,82 @@
+from dataclasses import dataclass
+from abc import *
+import pandas as pd
+import numpy as np
+@dataclass
+class File(object):
+    context: str
+    fname: str
+    dframe: object
+
+    @property
+    def context(self) -> str: return self._context
+
+    @context.setter
+    def context(self, context): self._context = context
+
+    @property
+    def fname(self) -> str: return self._fname
+
+    @fname.setter
+    def fname(self, fname): self._fname = fname
+
+    @property
+    def dframe(self) -> str: return self._dframe
+
+    @dframe.setter
+    def dframe(self, dframe): self._dframe = dframe
+
+class ReaderBase(metaclass=ABCMeta):
+
+    @abstractmethod
+    def new_file(self):
+        pass
+
+    @abstractmethod
+    def csv(self):
+        pass
+
+    @abstractmethod
+    def xls(self):
+        pass
+
+    @abstractmethod
+    def json(self):
+        pass
+
+class PrinterBase(metaclass=ABCMeta):
+
+    @abstractmethod
+    def dframe(self):
+        pass
+
+class Reader(ReaderBase):
+
+    def new_file(self, file) -> str:
+        return file.context + file.fname
+
+    def csv(self, file) -> object:
+        return pd.read_csv(f'{self.new_file((file))}.csv', encoding='UTF-8', thousands=',')
+
+    def xls(self):
+        pass
+
+    def json(self):
+        pass
+
+class Printer(PrinterBase):
+    def dframe(self, this):
+        print('*' * 100)
+        print(f'1. Target type \n {type(this)} ')
+        print(f'2. Target column \n {this.columns} ')
+        print(f'3. Target top 1개 행\n {this.head(1)} ')
+        print(f'4. Target bottom 1개 행\n {this.tail(1)} ')
+        print(f'4. Target null 의 갯수\n {this.isnull().sum()}개')
+        print('*' * 100)
+
+
+
+
+
+
+
